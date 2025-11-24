6af70876927db38680df5a2a6bbd621e039f770d
ayman <ayman.nait.sidi.moh@etu.univ-st-etienne.fr>
2025-11-24 11:27:56 +0100

    xxxx

--- src/Controller/SavoirplusController.php
+++ src/Controller/SavoirplusController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class SavoirplusController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/savoirplus', name: 'app_savoirplus')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('savoirplus/index.html.twig', [
+            'controller_name' => 'SavoirplusController',
+        ]);
     }
 }
